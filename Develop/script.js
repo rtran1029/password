61d70daa59b39e3d80eb6cc15e10227d809efdd7
rtran1029 <103476583+rtran1029@users.noreply.github.com>
2022-05-13 23:50:31 -0400

    making prompts and confirms

--- Develop/script.js
+++ Develop/script.js
@@ -3,14 +3,29 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  // Length
+  prompt('How long do you want it?')
+
+  // Include Lower case?
+  confirm('Include Lowercase Letters?')
+
+  // Include Upper case?
+  confirm('Include Uppercase Letters?')
+
+  // Include numbers?
+  confirm('Include Numbers?')
+
+  // Include Special Characters?
+  confirm('Include Special Characters?')
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+
 }
 
-// Add event listener to generate button
+// Add event listener to button
 generateBtn.addEventListener("click", writePassword);
-
-var length = prompt('How long do you want your password?')
